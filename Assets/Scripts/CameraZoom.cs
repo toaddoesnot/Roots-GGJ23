c91f953439db609c2d8386b7a97cee4fca82dff6
toaddoesnot <laiskusn7@gmail.com>
2023-01-30 20:39:43 -0500

    Basic Rotations and Camera
    You can press to and back to change era in the rooms, you can scroll in and out with a mouse wheel to see the middle room better.
    
    Next steps: zooms for other rooms, picking and snapping objects

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+    [SerializeField] float maxCameraDistance; //
+    CinemachineComponentBase componentBase;
+    float cameraDistance;
+    [SerializeField] float sensitivity = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (componentBase == null)
+        {
+            componentBase = virtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);
+            maxCameraDistance = (componentBase as CinemachineFramingTransposer).m_CameraDistance;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            print("scrolling");
+            cameraDistance = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+
+            if(componentBase is CinemachineFramingTransposer)
+            {
+                if (cameraDistance < 0)
+                {
+                    (componentBase as CinemachineFramingTransposer).m_CameraDistance -= cameraDistance;
+                    if ((componentBase as CinemachineFramingTransposer).m_CameraDistance > maxCameraDistance)
+                    {
+                        (componentBase as CinemachineFramingTransposer).m_CameraDistance = maxCameraDistance;
+                    }
+                }
+                else
+                {
+                    (componentBase as CinemachineFramingTransposer).m_CameraDistance -= cameraDistance;
+                    if ((componentBase as CinemachineFramingTransposer).m_CameraDistance < 2)
+                    {
+                        (componentBase as CinemachineFramingTransposer).m_CameraDistance = 2;
+                    }
+                }
+
+                   // (componentBase as CinemachineFramingTransposer).m_CameraDistance -= cameraDistance;
+            }
+
+        }
+    }
+}
