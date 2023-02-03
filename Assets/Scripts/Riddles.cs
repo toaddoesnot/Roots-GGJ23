82a0b601554fd39aae83ab60a1cf67ad639fbab9
toaddoesnot <laiskusn7@gmail.com>
2023-02-03 03:31:32 -0500

    Logic script ready
    Game can be won, still needs to be bug-proofed

--- Assets/Scripts/Riddles.cs
+++ Assets/Scripts/Riddles.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class Riddles : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject[] locks;
+    public int neutralEra;
 }
