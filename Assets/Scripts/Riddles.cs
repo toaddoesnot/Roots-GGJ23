4d7807459e30cc278a2f1c4d5aa1d3eab8075393
toaddoesnot <laiskusn7@gmail.com>
2023-02-05 02:13:15 -0500

    old logic to save

--- Assets/Scripts/Riddles.cs
+++ Assets/Scripts/Riddles.cs
@@ -12,12 +12,15 @@ public class Riddles : MonoBehaviour
     public Button toBut;
     public bool butsActive;
 
+    public GameObject referenceRoom;
+    public EraManager managerSc;
 
     void Update()
     {
         if (butsActive)
         {
             toBut.interactable = true;
+
             if (neutralEra > 0)
             {
                 backBut.interactable = true;
@@ -26,12 +29,32 @@ public class Riddles : MonoBehaviour
             {
                 backBut.interactable = false;
             }
+
+            if (referenceRoom.GetComponent<EraHolder>().currentEra is 2)
+            {
+                if (managerSc.sndWin is false)
+                {
+                    toBut.interactable = false;
+                }
+                else
+                {
+                    toBut.interactable = true;
+                }
+            }
+            else
+            {
+                toBut.interactable = true;
+            }
         }
         else
         {
             backBut.interactable = false;
             toBut.interactable = false;
         }
+
+       
+        //if room[0] era is 2 and 2nd win is false then disable a button
+
     }
 
 
